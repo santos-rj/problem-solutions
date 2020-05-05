f2d5325a20a2e90781a9a64c6f682eed0cb01ee5
Rafael Santos <raf535@outlook.com>
2020-05-04 21:22:03 -0300

    Update realizado por meio de script

---
+++ Treino para a OBI 2018/Problemas no SPOJ-BR/Pneu.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define endl '\n';
+
+using namespace std;
+
+int main()
+{
+
+    int n = 0, m = 0;
+
+    cin >> n >> m;
+
+    cout << (n - m) << endl;
+
+    return 0;
+}
