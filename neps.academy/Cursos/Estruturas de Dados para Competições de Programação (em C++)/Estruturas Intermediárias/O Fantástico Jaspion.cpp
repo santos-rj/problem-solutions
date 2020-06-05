4ab24344eb71eac775ee88206dcb7bc8034c0d32
Rafael Santos <raf535@outlook.com>
2020-06-05 16:33:46 -0300

    feat: resolutions created in 2020-06-05.

---
+++ neps.academy/Cursos/Estruturas de Dados para Competições de Programação (em C++)/Estruturas Intermediárias/O Fantástico Jaspion.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <map>
+
+#define desync                        \
+    ios_base::sync_with_stdio(false); \
+    cout.tie(NULL);                   \
+    cin.tie(NULL)
+#define endl '\n';
+
+using namespace std;
+
+int main()
+{
+
+    long long t, m, n, ant;
+    string inA, inB;
+    map<string, string> dicio;
+
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> m >> n;
+        cin.ignore();
+
+        for (int i = 0; i < m; i++)
+        {
+            getline(cin, inA);
+            getline(cin, inB);
+
+            dicio.insert(make_pair(inA, inB));
+        }
+
+        for (int i = 0, auth = 0; i < n; i++, auth = 0)
+        {
+            getline(cin, inA);
+            inB = "";
+            ant = 0;
+
+            int tamanho = inA.length();
+            for (int j = 0; j < tamanho; j++)
+            {
+                if (inA[j] != ' ')
+                {
+                    inB += inA[j];
+                    auth = 1;
+                }
+                else
+                {
+                    if (dicio[inB] != "")
+                    {
+                        if (ant != 0)
+                        {
+                            cout << " " << dicio[inB];
+                            ant++;
+                        }
+                        else
+                        {
+                            cout << dicio[inB];
+                            ant++;
+                        }
+                    }
+                    else
+                    {
+                        cout << inB;
+                        ant++;
+                    }
+                    inB = "";
+                    auth = 0;
+                }
+            }
+
+            if (auth == 1)
+            {
+                if (dicio[inB] != "")
+                {
+                    if (ant != 0)
+                    {
+                        cout << " " << dicio[inB];
+                        ant++;
+                    }
+                    else
+                    {
+                        cout << dicio[inB];
+                        ant++;
+                    }
+                }
+                else
+                {
+                    cout << inB;
+                    ant++;
+                }
+            }
+            cout << endl;
+        }
+
+        cout << endl;
+        dicio.clear();
+    }
+    return 0;
+}
