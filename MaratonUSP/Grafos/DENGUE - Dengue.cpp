7a450fab7a418ad4fb2029cc139602e2fbaa8948
Rafael Santos <raf535@outlook.com>
2020-06-16 15:03:33 -0300

    feat: resolutions created in 2020-06-16.

--- MaratonUSP/Grafos/DENGUE - Dengue.cpp
+++ MaratonUSP/Grafos/DENGUE - Dengue.cpp
@@ -6,30 +6,26 @@
 	ios_base::sync_with_stdio(false); \
 	cout.tie(NULL);                   \
 	cin.tie(NULL)
-#define endl '\n'
-#define INF 0x3f3f3f3f
-#define pb push_back
+#define endl '\n';
 
 using namespace std;
 
-typedef long long ll;
-
-vector<int> adj[102];
-
-int dist[102], max_dist = INT_MIN, ans, clk;
+int i, j, k;
+vector<int> adj[105];
+int distancia[105], maior_distancia = INT_MIN, ans, cont;
 
 void dfs(int v)
 {
 	for (int x : adj[v])
 	{
-		if (dist[x] != -1)
+		if (distancia[x] != -1)
 			continue;
-		dist[x] = dist[v] + 1;
+		distancia[x] = distancia[v] + 1;
 		dfs(x);
 	}
-	if (dist[v] > max_dist)
+	if (distancia[v] > maior_distancia)
 	{
-		max_dist = dist[v];
+		maior_distancia = distancia[v];
 	}
 }
 
@@ -39,40 +35,41 @@ int main()
 	desync;
 
 	int n;
+
 	cin >> n;
 
 	while (n != 0)
 	{
-
-		for (int i = 0; i < n - 1; i++)
+		for (i = 0; i < n - 1; i++)
 		{
 			int a, b;
 			cin >> a >> b;
-			adj[a].pb(b);
-			adj[b].pb(a);
+
+			adj[a].push_back(b);
+			adj[b].push_back(a);
 		}
 
-		int last_dist = INT_MAX;
+		int ultima_distancia = INT_MAX;
 
-		for (int i = 1; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			max_dist = INT_MIN;
-			for (int j = 1; j <= n; j++)
-				dist[j] = -1;
-			dist[i] = 0;
+			maior_distancia = INT_MIN;
+			for (j = 1; j <= n; j++)
+				distancia[j] = -1;
+			distancia[i] = 0;
 			dfs(i);
-			if (max_dist < last_dist)
+			if (maior_distancia < ultima_distancia)
 			{
 				ans = i;
-				last_dist = max_dist;
+				ultima_distancia = maior_distancia;
 			}
 		}
 
-		cout << "Teste " << ++clk << endl;
+		cout << "Teste " << ++cont << endl;
 		cout << ans << endl;
 		cin >> n;
 
-		for (int i = 1; i <= n; i++)
+		for (i = 1; i <= n; i++)
 			adj[i].clear();
 	}
 
